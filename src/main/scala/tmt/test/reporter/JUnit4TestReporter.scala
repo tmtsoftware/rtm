315419757caa4e0f87b4d0d021d70f00514cf029
Shubham Chauhan <shubham25may1998@gmail.com>
2022-03-22 14:40:05 +0530

    #000: Minor refactoring

--- src/main/scala/tmt/test/reporter/JUnit4TestReporter.scala
+++ src/main/scala/tmt/test/reporter/JUnit4TestReporter.scala
@@ -7,20 +7,20 @@ class JUnit4TestReporter extends RunListener {
   private var results: Set[StoryResult] = Set.empty
   private val parentPath                = (sys.env ++ sys.props).getOrElse("RTM_PATH", "./target/RTM")
   private val reportFile                = (sys.env ++ sys.props).getOrElse("OUTPUT_FILE", "/testStoryMapping.txt")
+  private val failedTest                = Description.createTestDescription("FAILED", "FAILED")
 
-  private val fail                                   = Description.createTestDescription("FAILED", "FAILED")
   override def testRunFinished(result: Result): Unit = {
     CommonUtil.generateReport(parentPath, reportFile, results)
     results = Set.empty
   }
 
   override def testFinished(description: Description): Unit = {
-    if (!description.getChildren.contains(fail)) addResult(description.getMethodName, TestStatus.PASSED)
+    if (!description.getChildren.contains(failedTest)) addResult(description.getMethodName, TestStatus.PASSED)
   }
 
   override def testFailure(failure: Failure): Unit = {
     addResult(failure.getDescription.getMethodName, TestStatus.FAILED)
-    failure.getDescription.addChild(fail)
+    failure.getDescription.addChild(failedTest)
   }
 
   override def testIgnored(description: Description): Unit = {
@@ -30,6 +30,5 @@ class JUnit4TestReporter extends RunListener {
   private def addResult(name: String, testStatus: String): Unit = {
     val storyResults = CommonUtil.javaTestParser(name, testStatus)
     results ++= storyResults
-    println(results)
   }
 }
