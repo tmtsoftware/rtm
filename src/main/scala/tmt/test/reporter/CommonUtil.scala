1db002cc90f283be3fdda8f41db91bae38211dd5
Shubham Chauhan <shubham25may1998@gmail.com>
2022-03-28 17:18:00 +0530

    #000: Chauhan: Handle parameterized test reporting

--- src/main/scala/tmt/test/reporter/CommonUtil.scala
+++ src/main/scala/tmt/test/reporter/CommonUtil.scala
@@ -22,19 +22,26 @@ object CommonUtil {
   def javaTestParser(name: String, testStatus: String): Array[StoryResult] = {
     val (testName, stories) = CommonUtil.getTestData(name, Separators.__)
 
-    // handle the data provider case (see CSW EventSubscriberTest for an example)
-    // if there is a (, take it out of stories list and append to test name
-    val (newStories, dataProvider) = stories.indexOf("(") match {
+    // handle parameterized test cases (see CSW JMatrixKeyTypeTest for an example)
+    // if there is a [, take it out of stories list and append to test name
+    val (newStories, dataProvider) = stories.indexOf("[") match {
       case -1  => (stories, "")
       case pos => (stories.take(pos), stories.takeRight(stories.length - pos))
     }
 
-    newStories
+    // handle the data provider case (see CSW EventSubscriberTest for an example)
+    // if there is a (, take it out of stories list and append to test name
+    val (storyTags, parameters) = newStories.indexOf("(") match {
+      case -1  => (newStories, "")
+      case pos => (newStories.take(pos), newStories.takeRight(stories.length - pos))
+    }
+
+    storyTags
       .drop(2)                      // Drop the "__"
       .split(Separators.UNDERSCORE) // multiple stories
       .sliding(2, 2)
       .map(x => x.mkString("-"))
-      .map(x => StoryResult(x.strip(), testName.strip() + dataProvider, testStatus))
+      .map(x => StoryResult(x.strip(), testName.strip() + parameters + dataProvider, testStatus))
       .toArray
   }
 }
